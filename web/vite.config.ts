b9e433bd477bf2a3c27fe5d0ad911b7123762d64
Ukubot Studios <ukubot14@gmail.com>
2026-08-18 08:33:07 -0700

    Add server proxy configuration to Vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   base: '/Atlas2.0/',
   plugins: [react()],
+
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+      '/health': 'http://localhost:3001',
+    },
+  },
 })
